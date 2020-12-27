6757d99b0324b0b639c6a1ccde21e421079dfd96
Maanav Garg <gargm1@mcmaster.ca>
2020-12-27 03:18:07 -0500

    adjusted server to new dataset

--- chrome-extension/inject.js
+++ chrome-extension/inject.js
@@ -2,7 +2,7 @@ var userData = new Array();
 const YEAR = 2020;
 
 (function () {
-    
+    // https://www.netflix.com/api/shakti/vb13b96d9/viewingactivity?pg=1&pgSize=100
     alert('Please wait a few moments');
     URI = 'https://www.netflix.com/api/shakti';
     buildID = '/vb13b96d9';
@@ -18,47 +18,38 @@ const YEAR = 2020;
         breakPoint = isRequired(userViewedItems);
         userData = [...userData, ...userViewedItems]
         i++;
-    } while (i < 6); // TESTING - Replace with `breakPoint` 
+    } while (i < 15); // TESTING - Replace with `breakPoint` 
     console.log(userData)
-
-
-
-
+    handleUserData(userData);
 })();
+
+
+function handleUserData(event){
+    const URI = "http://127.0.0.1:5000";
+    const formData = new FormData();
+    console.log(JSON.stringify(event))
+    formData.append('userData', JSON.stringify(event));
+    formData.append('timeFrame', "2020");
+    const requestOptions = {
+        method: 'POST',
+        body: formData,
+    };
+    fetch(`${URI}/get_data`, requestOptions)
+    .then(function(response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+    .catch(function(error) {
+        console.log('error');
+    });
+};
 
 
 function isRequired(userViewedItems){
     const item = userViewedItems[userViewedItems.length - 1]
-    console.log(item)
     const itemDate = new Date(item['date'])
     return itemDate.getFullYear() >= YEAR;
 }
-
-// for (i = 1; i < 5; i++) {
-//     reqURI = `${URI}${buildID}/viewingactivity?pg=${i}&pgSize=20`;
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.open("GET", reqURI, false);
-//     xmlHttp.send(null);
-//     userViewedItems = JSON.parse(xmlHttp.responseText).viewedItems
-//     if(!Array.isArray(userViewedItems) || !userViewedItems.length){
-//         break;
-//     }
-//     appendData(JSON.parse(xmlHttp.responseText).viewedItems)
-// }
-
-// var i = 0;
-// while (true) {
-//     reqURI = `${URI}${buildID}/viewingactivity?pg=${i}&pgSize=100`;
-//     var xmlHttp = new XMLHttpRequest();
-//     xmlHttp.open("GET", reqURI, false);
-//     xmlHttp.send(null);
-//     userViewedItems = JSON.parse(xmlHttp.responseText).viewedItems;
-//     if(!Array.isArray(userViewedItems) || !userViewedItems.length){
-//         break;
-//     }
-//     console.log(userViewedItems)
-//     appendData(userViewedItems)
-//     i++;
-// }
-
-    // document.getElementsByClassName("bd")[0].innerHTML = dashboard;
